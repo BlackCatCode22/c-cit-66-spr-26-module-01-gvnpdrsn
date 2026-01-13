d3acd64065648bd947fb40bf4a4dceeb6659e0c2
gvnpdrsn <gavgavp8@gmail.com>
2026-01-13 13:40:54 -0800

    Add files via upload

---
+++ sumint.cpp
@@ -0,0 +1,13 @@
+//sumint.cpp
+// Created by GP on 1/13/2026.
+
+#include <iostream>
+int main() {
+    int num1, num2;
+    std::cout << "Enter two numbers: ";
+    std::cin >> num1 >> num2;
+    std::cout << num1 + num2 << '\n';
+
+
+    return 0;
+}
