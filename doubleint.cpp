c2c38d589b6873f22830cb19e612c5a9ede3a56b
gvnpdrsn <gavgavp8@gmail.com>
2026-01-13 13:30:44 -0800

    Add files via upload

---
+++ doubleint.cpp
@@ -0,0 +1,14 @@
+//doubleint.cpp
+// Created by GP on 1/13/2026.
+#include <iostream>
+
+int main() {
+    std::cout <<"Enter an integer:";
+
+    int num{}; //define variable num as an integer variable
+    std::cin >>num; //get integer from keyboard
+
+    std::cout<<"Double that number is:" << num*2 << '\n';
+
+    return 0;
+}
