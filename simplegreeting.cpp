3a4426fb71d4f9d6076a38f8c8b35818f3568aff
gvnpdrsn <gavgavp8@gmail.com>
2026-01-13 13:52:13 -0800

    Add files via upload

---
+++ simplegreeting.cpp
@@ -0,0 +1,14 @@
+//simplegreeting.cpp
+// Created by GP on 1/13/2026.
+
+#include <iostream>
+#include <string>
+
+int main() {
+   std::string name;
+    std::cout << "Enter Your Name:";
+    std::cin >> name;
+
+    std::cout<< "Greetings "  << name <<  " Have a good day!";
+    return 0;
+}
